b97f09fdfdcec38d83a58588f1ef3595dc1cee58
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-18 12:33:06 +0300

    Add environment configuration and enhance server setup with middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import mongoconnection from "./db/mongoconnection.js";
+import autoApply from "./routes/aply.route.js";
+import authRoutes from "./routes/user.route.js";
+import linksToMogoDbUrl from "./db/linksToMogoDbUrl.js";
 
-
-
-// initialize express
+// initialization
 const app = express();
 
-console.log('Hello World');
+app.use(
+  cors({
+    origin: ["",""],
+    credentials: true,
+    allowedHeaders: "*", 
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
+  })
+);
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(PORT, () => {
+  linksToMogoDbUrl();
+  console.log(`server is running on port ${PORT}`);
+});
+
+//   Aidding soem APis
+app.use("/api", autoApply);
