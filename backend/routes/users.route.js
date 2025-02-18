5d23c4e77f77961e0bf9b0cc6dc4d899a1c0ff4c
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-18 13:50:41 +0300

    Add user signin functionality and update JWT secret key in signin controller

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { signup } from '../controllers/user.controllers.js';
+import { signin, signup } from '../controllers/user.controllers.js';
 
 // Initialize express router
 const router = express.Router();
 
 // Set default API response
 router.post('/register', signup)
+router.post('/signin', signin)
 
 // Export API routes
 export default router;
