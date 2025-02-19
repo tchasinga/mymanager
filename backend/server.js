e44d167c870298ee850b8139c86aa20e9fb68567
Tchasinga jacques <tchasingajacques@gmail.com>
2025-02-19 06:43:57 +0300

    Refactor server.js to improve structure and serve static files from the frontend build

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import dbconnection from "./db/dbconnection.js";
 import autoApplyForUser from "./routes/users.route.js";
 import autoUserTask from "./routes/task.route.js";
-import path from "path";
 
-// initialization
+// Load environment variables
+dotenv.config();
+
+// Initialize Express app
 const app = express();
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
+// Middleware
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://mymanagersite.onrender.com"],
@@ -18,25 +24,23 @@ app.use(
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"],
   })
 );
-
-dotenv.config();
-const PORT = process.env.PORT;
-const __dirname = path.resolve();
-
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(PORT, () => {
-    dbconnection();
-  console.log(`server is running on port ${PORT}`);
-});
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+// API routes
+app.use("/apis/auth", autoApplyForUser); 
+app.use("/api/tasks", autoUserTask);
 
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
-})
+});
 
-//   Aidding soem APis
- app.use("/apis/auth", autoApplyForUser); 
- app.use("/api/tasks", autoUserTask);
+// Start server and connect to database
+app.listen(PORT, () => {
+  dbconnection();
+  console.log(`Server is running on port ${PORT}`);
+});
